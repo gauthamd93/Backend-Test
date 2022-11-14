6c33132ed196ea53b70ff5f16b9d69114e6f88bc
gowtham <gowtham.dhananjayan@gmail.com>
2022-11-14 13:28:46 +0530

    WIP

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.info("hi");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // handling multiple ports , error
+
+const app = express();
+const router = express.Router();
+
+app.use(cors());
+
+app.use(
+    router.get('/', (req, res) => {
+      // request response handler
+      res.send({
+        status: 200,
+        message: "Hiii",
+        author: "Jane Doe"
+      });
+    })
+  );
+
+const port = 9000;
+app.listen(port, () => {
+  console.log('running');
+});
